f261283a33c0a8fa3b28fe5e883fd2a7c20908f0
Mubaraq Wahab <48601635+mubaraqwahab@users.noreply.github.com>
2023-05-11 15:19:51 +0000

    fix postgres fk unique error in migrations

---
+++ database/migrations/2023_05_11_151152_make_task_uuid_primary_key.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create("tasks", function (Blueprint $table) {
+            $table->primary("id");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::create("tasks", function (Blueprint $table) {
+            $table->dropPrimary("id");
+        });
+    }
+};
