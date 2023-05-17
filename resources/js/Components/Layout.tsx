5fbb4d6ff3f24b428be4f74cf6b1e5c5fc0c3b58
Mubaraq Wahab <mub.wahab26@gmail.com>
2023-05-17 22:51:55 +0100

    make the layout dropdown a bit prettier

--- resources/js/Components/Layout.tsx
+++ resources/js/Components/Layout.tsx
@@ -1,7 +1,7 @@
 import { AuthContext } from "@/context";
 import { PageProps } from "@/types";
 import { Head } from "@inertiajs/react";
-import { PropsWithChildren } from "react";
+import { PropsWithChildren, useId } from "react";
 import Form from "@/Components/Form";
 import * as DropdownMenu from "@radix-ui/react-dropdown-menu";
 
@@ -11,25 +11,16 @@ type LayoutProps = PropsWithChildren<{
 }>;
 
 export default function Layout({ auth, title, children }: LayoutProps) {
+  const logOutFormId = useId();
+  const dropdownMenuItemClass =
+    "px-2 py-1 block w-full text-left rounded-md hover:bg-gray-100";
+
   return (
     <AuthContext.Provider value={auth}>
       <Head title={title} />
       <header className="border-b">
         <div className="container flex justify-between items-center py-3">
           <a href="/">Tasks</a>
-          {/* <details>
-            <summary>{auth.user.name}</summary>
-            <ul>
-              <li>
-                <a href="/account">Account</a>
-              </li>
-              <li>
-                <Form method="POST" action={route("logout")}>
-                  <button type="submit">Log out</button>
-                </Form>
-              </li>
-            </ul>
-          </details> */}
 
           <DropdownMenu.Root>
             <DropdownMenu.Trigger asChild>
@@ -37,27 +28,38 @@ export default function Layout({ auth, title, children }: LayoutProps) {
             </DropdownMenu.Trigger>
 
             <DropdownMenu.Portal>
-              <DropdownMenu.Content align="end">
-                <DropdownMenu.Item asChild>
+              <DropdownMenu.Content
+                align="end"
+                sideOffset={12}
+                className="w-48 border rounded-md shadow-lg p-2 bg-white"
+              >
+                <DropdownMenu.Item asChild className={dropdownMenuItemClass}>
                   {/* TODO: icon */}
                   <a href="/account">Account</a>
                 </DropdownMenu.Item>
 
-                <DropdownMenu.Item asChild>
+                <DropdownMenu.Item asChild className={dropdownMenuItemClass}>
                   {/* TODO: icon */}
                   <a href="/TODO">Completed tasks</a>
                 </DropdownMenu.Item>
 
-                <DropdownMenu.Separator />
-                <DropdownMenu.Item asChild>
+                <DropdownMenu.Separator className="border-t my-2" />
+
+                <DropdownMenu.Item asChild className={dropdownMenuItemClass}>
                   {/* TODO: icon */}
-                  <Form method="POST" action={route("logout")}>
-                    <button type="submit">Log out</button>
-                  </Form>
+                  <button type="submit" form={logOutFormId}>
+                    Log out
+                  </button>
                 </DropdownMenu.Item>
               </DropdownMenu.Content>
             </DropdownMenu.Portal>
           </DropdownMenu.Root>
+          <Form
+            method="POST"
+            action={route("logout")}
+            id={logOutFormId}
+            className="sr-only"
+          ></Form>
         </div>
       </header>
       <main>
