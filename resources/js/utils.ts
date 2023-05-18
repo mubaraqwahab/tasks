ab56e1e449f35ae1d8289eeee48ff90eab0084cd
Mubaraq Wahab <48601635+mubaraqwahab@users.noreply.github.com>
2023-05-18 10:56:25 +0000

    ignore the value missing and too long validity states

--- resources/js/utils.ts
+++ resources/js/utils.ts
@@ -14,6 +14,7 @@ export function p<E extends FormEvent>(listener: (e: E) => void) {
 
 export const NOT_WHITESPACE_ONLY_PATTERN = String.raw`\s*\S(.*\S)?\s*`;
 
+
 export const taskNameInputValidationProps: JSX.IntrinsicElements["input"] = {
   type: "text",
   required: true,
@@ -22,7 +23,11 @@ export const taskNameInputValidationProps: JSX.IntrinsicElements["input"] = {
   pattern: String.raw`\s*\S(.*\S)?\s*`,
   onInvalid: (e) => {
     const input = e.target as HTMLInputElement;
-    // Don't worry about other errors, as the browser's default messages suffice
+    // Don't worry about other errors, as the browser's default messages suffice.
+    // Note that the input validity states are not mutually exclusive. So the
+    // validity.valueMissing and validity.patternMismatch states could both be true.
+    if (input.validity.valueMissing || input.validity.tooLong) return
+
     if (input.validity.patternMismatch) {
       input.setCustomValidity("The task name can't be just whitespace");
       input.reportValidity();
