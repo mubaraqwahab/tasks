0526021242b3477d2bbff272019a103c861722e9
Mubaraq Wahab <mub.wahab26@gmail.com>
2023-06-25 16:24:13 +0100

    Add user:list and user:list artisan commands

--- routes/console.php
+++ routes/console.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
@@ -17,3 +18,21 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command("inspire", function () {
     $this->comment(Inspiring::quote());
 })->purpose("Display an inspiring quote");
+
+Artisan::command("user:list", function () {
+    $this->table(["Name", "Email"], User::all(["name", "email"])->toArray());
+})->purpose("List all users.");
+
+Artisan::command("user:delete {email}", function (string $email) {
+    $user = User::query()->firstWhere("email", $email);
+    if (!$user) {
+        $this->warn("No user exists with the email $email.");
+    } elseif (
+        $this->confirm(
+            "Are you sure you want to delete the following user:\n {$user->name} {$user->email}?",
+        )
+    ) {
+        $user->delete();
+        $this->info("User $email has been deleted.");
+    }
+})->purpose("Delete a user by email.");
