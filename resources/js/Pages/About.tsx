6c808d42f211f3f414a05750b28f7b4fa9465095
Mubaraq Wahab <mub.wahab26@gmail.com>
2023-06-29 20:03:51 +0100

    more about

--- resources/js/Pages/About.tsx
+++ resources/js/Pages/About.tsx
@@ -133,6 +133,8 @@ export default function About() {
             of this app as well as some challenges I faced and lessons I learnt.
           </p>
 
+          <p>NOTE: everything below this paragraph is still an early draft!</p>
+
           <p>TODO: credits here?</p>
 
           <p>TODO: TOC here?</p>
@@ -146,35 +148,117 @@ export default function About() {
             My initial plan was to create a traditional server-rendered app,
             where actions like adding a new task would trigger a page reload to
             save the task to the server and show the task on the page. Soon
-            after completing this, I found the user experience to be terrible,
-            so I decided to improve it with a Todoist-style optimistic UI.
+            after completing this, I found the user experience to be terrible
+            and decided to improve it with an optimistic UI.
           </p>
 
           <p>
-            TODO: reverse engineering Todoist's optimistic UI... How my
-            implementation is a modified form of Todoist's... (Maybe add this to
-            the previous paragraph?)
+            The optimistic UI in this app is based on my observations while
+            reverse-engineering Todoist in my browser's DevTools. It works thus:
           </p>
 
-          <p>TODO: Overview of my implementation (end to end)</p>
-
-          <p>Related stuff</p>
+          <p>On the client side:</p>
           <ul>
-            <li>UUIDs for task (and change?) IDs</li>
             <li>
-              Switcing from Blade to Inertia to reuse server components on front
-              end and for reactive UI
+              The app saves changes in an array (which I call a{" "}
+              <dfn>changelog</dfn>) in local storage and updates the UI. [Sample
+              changelog]
             </li>
-            <li>How seemingly trivial features invite complexity</li>
+            <li>
+              The app attempts to sync the changelog to the server if you're
+              online.
+            </li>
+            <li>
+              If you're offline, then the app waits until you're back online to
+              sync. In the meantime, it continues pushing any new changes you
+              make to the changelog and updating the UI.
+            </li>
+            <li>
+              The server's response to a sync request typically looks thus:
+              [Sample response]. On receiving this, the app removes those ok
+              changes from the changelog.
+            </li>
+            <li>
+              In the event that there's a sync conflict or validation error, the
+              server responds thus: [Sample response]. And the app keeps the
+              failed changes in the changelog, updates them thus: [Updated
+              changelog]. Then the app alerts you: [Screenshot of alert dialog].
+              A sync conflict could occur if, say, you're using multiple
+              devices, like your phone and laptop, to access the app, and the
+              following sequence of events occur: [Sequence of events on phone
+              and laptop]
+            </li>
+            <li>
+              Other errors can (could?) occur during a sync: e.g. a network
+              error or a server error. The app subtly notifies you of these and
+              gives you the option to retry syncing.
+            </li>
           </ul>
 
-          <hr />
+          <p>On the server side:</p>
           <ul>
-            <li>The optimistic UI</li>
-            <li>Declarative, reactive UI...</li>
+            <li>
+              The app validates each change in the received changelog, saves the
+              change to a <code>task_changes</code> database table and applies
+              the change to a <code>tasks</code> table, where the tasks live. It
+              does the latter two in a transaction to avoid inconsistencies when
+              either fails (e.g. due to a sync conflict.)
+            </li>
+            <li>
+              Then the server responds with a relevant result, as previously
+              described.
+            </li>
+          </ul>
+
+          <p>Some important, finer points:</p>
+          <ul>
+            <li>
+              The ids of the tasks and changes are UUIDs to ensure they never
+              clash when generated on different devices. I learnt UUIDs are
+              designed such that no device ever generates the same UUID twice,
+              and no two devices ever generate the same UUID. (I also learnt
+              that UUIDs <em>do</em> clash in some situations, but that's beyond
+              the scope of this writing.)
+            </li>
+            <li>
+              The changes are persisted server-side just to prevent duplicates.
+              In some situations, the client may send the same changes twice to
+              the server. The changes aren't all idempotent, so the server
+              persists them to identify and avoid re-applying duplicate changes.
+              The response for duplicate changes is [Sample response]. The
+              client treats these results as it does ok results.
+            </li>
+          </ul>
+
+          <h2>Switching to Inertia</h2>
+
+          <p>
+            Before the optimistic UI, I used Laravel's Blade templating engine
+            to render the frontend of the app on the server, and sprinkles of JS
+            for the interactivity on the client. And I continued thus when I
+            started implementing the optimistic UI. However, I later switched to
+            using React (via Inertia) and XState for two important reasons:
+          </p>
+
+          <ul>
+            <li>
+              Reusing components created on the server (via Blade) on the client
+              side
+            </li>
+            <li>
+              Expressing the logic of creating/deleting/modifying DOM nodes in
+              response to user actions and server responses.
+            </li>
+          </ul>
+
+          <p>How seemingly trivial features invite complexity</p>
+
+          <h3>Miscellaneous</h3>
+
+          <ul>
             <li>Interface design</li>
-            <li>Scoping requirements...</li>
             <li>Accessibility in Inertia</li>
+            <li>Scoping requirements...</li>
           </ul>
         </div>
       </main>
